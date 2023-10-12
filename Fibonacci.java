d7afa40dc497df7a120bf737fb7aed8e07924aa3
ArianTecnm <143622352+ArianTecnm@users.noreply.github.com>
2023-10-11 20:26:13 -0600

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+package fibonacci;
+import java.util.Scanner;
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introdusca un Numero");
+        
+    int Serie = scanner.nextInt() , Num1=0, Num2=1, Suma=1;
+    for(int i = 1; i<=Serie; i++) {
+        System.out.println("La Secuencia es:" + Suma);
+        Suma = Num1 + Num2;
+        Num1 =  Num2;
+        Num2 = Suma;
+    }
+    }
+    
+}
